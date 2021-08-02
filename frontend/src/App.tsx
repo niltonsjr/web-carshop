c07844dea9430bfcafc04bc1018c3d804fc7bf90
Nilton da Silva Junior <tonsti@gmail.com>
2021-08-02 21:06:54 +0200

    Adding Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import './assets/styles/custom.scss';
 import './App.css';
+
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Hello CarShop</h1>
+    <Navbar />
   );
 }
 
